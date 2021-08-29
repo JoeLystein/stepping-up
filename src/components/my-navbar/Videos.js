f798659c302d92ace940c9ff1ddb03b046acc778
Joel <>
2021-08-28 22:58:03 -0500

    videos page updated

--- src/components/my-navbar/Videos.js
+++ src/components/my-navbar/Videos.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import { YoutubePlayer} from 'reactjs-media'; 
 
-function Videos() {
+const Videos = () =>{
     return (
-        <div>
-            videos lists
-            <article>
-                <video src="" alt="video 1"/>
-            </article>
-        </div>
-    )
-}
+        <>
+        <YoutubePlayer
+            src="https://www.youtube.com/watch?v=iInWyhmQ6YE"
+            // poster="../assets/Homepage.jpeg"
+            width={650}
+            height={600}
+        />
+
+        <YoutubePlayer
+            src="https://www.youtube.com/watch?v=1eWeheJFBF4"
+            // poster="../assets/Homepage.jpeg"
+            width={650}
+            height={600}
+        />
+        </>
+    );
+};
 
 export default Videos; 
